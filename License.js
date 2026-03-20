04d4c5a0dcfbcf9fc46c799ea895b07c723e3684
nowakdeclan-bit <nowakdeclan@gmail.com>
2026-03-20 18:17:49 -0400

    Add custom license for School Club Organizer
    Added a custom license for the School Club Organizer project outlining usage restrictions and permission request process.

---
+++ License.js
@@ -0,0 +1,22 @@
+# Custom License for School Club Organizer
+
+Copyright (c) 2026 Declan Nowak
+
+Permission is hereby granted to any person obtaining a copy of this software 
+to view and fork this repository for personal, non-commercial use. 
+
+### Usage Restrictions
+You may NOT modify, redistribute, or create public derivative works (branches) 
+of this software without prior written confirmation from the author.
+
+### How to Request Permission
+To request permission to edit or branch this project, please send an email to:
+**nowakdeclan@gmail.com**
+
+Please include:
+1. Your GitHub username
+2. A brief description of the changes or features you want to add
+3. The intended purpose of your version
+
+Once you receive a confirmation email, you are permitted to proceed with your 
+proposed changes under the terms discussed.
